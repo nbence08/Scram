f091743d0b18a102593298910e97292b5b705cda
Nagy Bence <nbence08@gmail.com>
2022-02-07 17:07:07 +0100

    change Matrix constructors

--- core/math/Matrix4.h
+++ core/math/Matrix4.h
@@ -9,14 +9,17 @@ class Matrix4{
 
 public:
 	Matrix4(
-		real_t x1 = 0.0f, real_t x2 = 0.0f, real_t x3 = 0.0f, real_t x4 = 0.0f,
+		real_t x1, real_t x2, real_t x3, real_t x4,
 		real_t y1 = 0.0f, real_t y2 = 0.0f, real_t y3 = 0.0f, real_t y4 = 0.0f,
 		real_t z1 = 0.0f, real_t z2 = 0.0f, real_t z3 = 0.0f, real_t z4 = 0.0f,
 		real_t w1 = 0.0f, real_t w2 = 0.0f, real_t w3 = 0.0f, real_t w4 = 0.0f);
 
+
 	Matrix4(const Matrix3& mat3);
 
 	Matrix4(const real_t diag);
+
+	inline Matrix4(): Matrix4(1.0) {}
 
 	//takes three vectors as column vectors
 	Matrix4(Vector4 v1, Vector4 v2, Vector4 v3, Vector4 v4);
