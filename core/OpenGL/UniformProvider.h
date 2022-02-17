58a557a70b63840612daf0ae867f6e2068c203d2
Nagy Bence <nbence08@gmail.com>
2022-02-17 15:11:23 +0100

    move definitions to UniformProvider.cpp

--- core/OpenGL/UniformProvider.h
+++ core/OpenGL/UniformProvider.h
@@ -28,19 +28,10 @@ class UniformProvider{
 	/// </summary>
 	/// <param name="name">Name of the uniform in the shader, whose location is to be queried</param>
 	/// <returns>Location identifier of the uniform</returns>
-	unsigned int getUniformLocation(std::string name) {
-		if (locationCache.find(name) == end(locationCache)) {
-			unsigned int location = glGetUniformLocation(programId, name.c_str());
-			locationCache.insert({name, location});
-			return location;
-		}
-		else {
-			return locationCache[name];
-		}
-	}
+	unsigned int getUniformLocation(std::string name);
 public:
-	UniformProvider() { programId = 0; }
-	UniformProvider(unsigned int programId):programId(programId) {}
+	inline UniformProvider() { programId = 0; }
+	inline UniformProvider(unsigned int programId):programId(programId) {}
 
 	/// <summary>
 	/// Sets bool uniform value, identified by the name specified as parameter, in the 
@@ -48,9 +39,7 @@ public:
 	/// </summary>
 	/// <param name="name">Name of the uniform variable in the shader</param>
 	/// <param name="value">bool value set as uniform</param>
-	void setUniform(std::string name, bool value) {
-		setUniform(name, (int)value);
-	}
+	void setUniform(std::string name, bool value);
 
 	/// <summary>
 	/// Sets float uniform value, identified by the name specified as parameter, in the 
@@ -58,11 +47,7 @@ public:
 	/// </summary>
 	/// <param name="name">Name of the uniform variable in the shader</param>
 	/// <param name="value">float value set as uniform</param>
-	void setUniform(std::string name, float value) {
-		unsigned int location = getUniformLocation(name);
-		
-		glUniform1f(location, value);
-	}
+	void setUniform(std::string name, float value);
 
 	/// <summary>
 	/// Sets double uniform value, identified by the name specified as parameter, in the 
@@ -70,11 +55,7 @@ public:
 	/// </summary>
 	/// <param name="name">Name of the uniform variable in the shader</param>
 	/// <param name="value">double value set as uniform</param>
-	void setUniform(std::string name, double value) {
-		unsigned int location = getUniformLocation(name);
-
-		glUniform1d(location, value);
-	}
+	void setUniform(std::string name, double value);
 
 	/// <summary>
 	/// Sets vec2 uniform value, identified by the name specified as parameter, in the 
@@ -83,15 +64,7 @@ public:
 	/// <param name="name">Name of the uniform variable in the shader</param>
 	/// <param name="x">real value set as first coordinate of vec2 uniform</param>
 	/// <param name="y">real value set as second coordinate of vec2 uniform</param>
-	void setUniform(std::string name, const real_t x, const real_t y) {
-		unsigned int location = getUniformLocation(name);
-		if (GL_REAL == GL_FLOAT) {
-			glUniform2f(location, x, y);
-		}
-		else if (GL_REAL == GL_DOUBLE) {
-			glUniform2d(location, x, y);
-		}
-	}
+	void setUniform(std::string name, const real_t x, const real_t y);
 
 	/// <summary>
 	/// Sets vec3 uniform value, identified by the name specified as parameter, in the 
@@ -100,15 +73,7 @@ public:
 	/// </summary>
 	/// <param name="name">Name of the uniform variable in the shader</param>
 	/// <param name="value">Vector3 value set as uniform</param>
-	void setUniform(std::string name, const Vector3& value) {
-		unsigned int location = getUniformLocation(name);
-		if (GL_REAL == GL_FLOAT) {
-			glUniform3f(location, value.x, value.y, value.z);
-		}
-		else if (GL_REAL == GL_DOUBLE) {
-			glUniform3d(location, value.x, value.y, value.z);
-		}
-	}
+	void setUniform(std::string name, const Vector3& value);
 
 	/// <summary>
 	/// Sets vec4 uniform value, identified by the name specified as parameter, in the 
@@ -117,15 +82,7 @@ public:
 	/// </summary>
 	/// <param name="name">Name of the uniform variable in the shader</param>
 	/// <param name="value">Vector4 value set as uniform</param>
-	void setUniform(std::string name, const Vector4& value) {
-		unsigned int location = getUniformLocation(name);
-		if (GL_REAL == GL_FLOAT) {
-			glUniform4f(location, value.x, value.y, value.z, value.w);
-		}
-		else if (GL_REAL == GL_DOUBLE) {
-			glUniform4d(location, value.x, value.y, value.z, value.w);
-		}
-	}
+	void setUniform(std::string name, const Vector4& value);
 
 	/// <summary>
 	/// Sets mat4 uniform value, identified by the name specified as parameter, in the 
@@ -134,16 +91,7 @@ public:
 	/// </summary>
 	/// <param name="name">Name of the uniform variable in the shader</param>
 	/// <param name="value">Matrix4 value set as uniform</param>
-	void setUniform(std::string name, const Matrix4& value) {
-		unsigned int location = getUniformLocation(name);
-
-		if (GL_REAL == GL_FLOAT) {
-			glUniformMatrix4fv(location, 1, GL_FALSE, reinterpret_cast<const GLfloat*>(&value));
-		}
-		else if (GL_REAL == GL_DOUBLE) {
-			glUniformMatrix4dv(location, 1, GL_FALSE, reinterpret_cast<const GLdouble*>(&value));
-		}
-	}
+	void setUniform(std::string name, const Matrix4& value);
 
 	/// <summary>
 	/// Sets int uniform value, identified by the name specified as parameter, in the 
@@ -151,11 +99,7 @@ public:
 	/// </summary>
 	/// <param name="name">Name of the uniform variable in the shader</param>
 	/// <param name="value">int value set as uniform</param>
-	void setUniform(std::string name, const int value) {
-		unsigned int location = getUniformLocation(name);
-
-		glUniform1i(location, value);
-	}
+	void setUniform(std::string name, const int value);
 
 	/// <summary>
 	/// Sets texture unit value, identified by the name specified as parameter, in the 
@@ -163,11 +107,7 @@ public:
 	/// </summary>
 	/// <param name="name">Name of the sampler in the shader</param>
 	/// <param name="unit">Id of the texture unit to be set</param>
-	void setTexture(std::string samplerName, const TextureUnit& unit) {
-		unsigned int location = getUniformLocation(samplerName);
-
-		glUniform1i(location, unit.getUnitNum());
-	}
+	void setTexture(std::string samplerName, const TextureUnit& unit);
 
 	/// <summary>
 	/// Sets point light source in the shader. Performs shadow map setting as well if
@@ -176,19 +116,7 @@ public:
 	/// <param name="light">Point light source to bet set</param>
 	/// <param name="index">Index of the point light source to be set in the shader, in the array of point lights</param>
 	/// <param name="arrayName">Name of the array containing the point light data in the shader</param>
-	void setLight(const PointLight& light, int index, std::string arrayName = "pointLights") {
-		setUniform(arrayName + "[" + std::to_string(index) + "].intensity", light.intensity);
-		setUniform(arrayName + "[" + std::to_string(index) + "].attenuation", light.attenuation);
-		setUniform(arrayName + "[" + std::to_string(index) + "].position", light.position);
-		if (light.hasShadowMap()) {
-			setUniform(arrayName + "ShadowMap[" + std::to_string(index) + "]", light.shadowMap->getTextureUnitNum());
-			auto lightSpaceMatrices = light.getLightSpaceMatrices();
-			setUniform(arrayName + "[" + std::to_string(index) + "].farPlane", light.farPlane);
-			for (int i = 0; i < 6; i++) {
-				setUniform(arrayName + "[" + std::to_string(index) + "].lightMatrices["+std::to_string(i)+"]", lightSpaceMatrices[i]);
-			}
-		}
-	}
+	void setLight(const PointLight& light, int index, std::string arrayName = "pointLights");
 
 	/// <summary>
 	/// Sets directional light source in the shader. Performs shadow map setting as well if
@@ -197,14 +125,7 @@ public:
 	/// <param name="light">Directional light source to bet set</param>
 	/// <param name="index">Index of the directional light source to be set in the shader, in the array of point lights</param>
 	/// <param name="arrayName">Name of the array containing the directional light data in the shader</param>
-	void setLight(const DirectionalLight& light, int index, std::string arrayName = "dirLights") {
-		setUniform(arrayName + "[" + std::to_string(index) + "].intensity", light.intensity);
-		setUniform(arrayName + "[" + std::to_string(index) + "].direction", light.direction.normalized());
-		setUniform(arrayName + "[" + std::to_string(index) + "].lightMatrix", light.getLightSpaceMatrix());
-		if (light.hasShadowMap()) {
-			setUniform(arrayName + "[" + std::to_string(index) + "].shadowMap", light.shadowMap->getTextureUnitNum());
-		}
-	}
+	void setLight(const DirectionalLight& light, int index, std::string arrayName = "dirLights");
 
 	/// <summary>
 	/// Same as overload of setLight for directional lights.
@@ -214,9 +135,7 @@ public:
 	/// <param name="light">Point light source to bet set</param>
 	/// <param name="index">Index of the point light source to be set in the shader, in the array of point lights</param>
 	/// <param name="arrayName">Name of the array containing the point light data in the shader</param>
-	void setUniform(const DirectionalLight& light, int index, std::string arrayName = "dirLights") {
-		setLight(light, index, arrayName);
-	}
+	void setUniform(const DirectionalLight& light, int index, std::string arrayName = "dirLights");
 
 	/// <summary>
 	/// Same as overload of setLight for point lights.
@@ -226,9 +145,7 @@ public:
 	/// <param name="light">Point light source to bet set</param>
 	/// <param name="index">Index of the point light source to be set in the shader, in the array of point lights</param>
 	/// <param name="arrayName">Name of the array containing the point light data in the shader</param>
-	void setUniform(const PointLight& light, int index, std::string arrayName = "pointLights") {
-		setLight(light, index, arrayName);
-	}
+	void setUniform(const PointLight& light, int index, std::string arrayName = "pointLights");
 
 	//TODO: add setLight for SpotLights
 
@@ -238,49 +155,7 @@ public:
 	/// <param name="material">Material object to be set</param>
 	/// <param name="index">Index of material to be set in the shader</param>
 	/// <param name="arrayName">Name of the arra containing the materials in the shader.</param>
-	void setMaterial(const Material& material,int index, std::string arrayName = "materials") {
-		bool albedo_is_texture;
-		if (material.albedo.index() == 0) { //Vector3
-			
-			Vector3 albedo = std::get<Vector3>(material.albedo);
-			albedo_is_texture = false;
-
-			setUniform(arrayName + "[" + std::to_string(index) + "].albedoTexture", 0);
-			setUniform(arrayName + "[" + std::to_string(index) + "].albedoVec", albedo);
-		}
-		else { //Texture
-			
-			Texture2D& albedo = *std::get<std::shared_ptr<Texture2D>>(material.albedo);
-			auto textureUnit = albedo.getTextureUnitNum();
-			if (textureUnit == -1) {
-				//TODO: automatic texture unit creation could be implemented in the future
-				Vector3 albedo(1.0, 0.0, 1.0);
-				albedo_is_texture = false;
-
-				setUniform(arrayName + "[" + std::to_string(index) + "].albedoTexture", 0);
-				setUniform(arrayName + "[" + std::to_string(index) + "].albedoVec", albedo);
-			}
-			albedo_is_texture = true;
-			setUniform(arrayName + "[" + std::to_string(index) + "].albedoVec", Vector3(0.0, 0.0, 0.0));
-			setUniform(arrayName + "[" + std::to_string(index) + "].albedoTexture", textureUnit);
-		}
-
-		setUniform(arrayName + "[" + std::to_string(index) + "].albedo_is_texture", albedo_is_texture);
-		setUniform(arrayName + "[" + std::to_string(index) + "].smoothness", material.smoothness);
-		setUniform(arrayName + "[" + std::to_string(index) + "].metalness", material.metalness);
-
-		if (material.hasEmission()) {
-			if (material.hasEmissionTexture()) {
-				const auto tex = std::get<std::shared_ptr<Texture2D>>(material.emission);
-				setUniform(arrayName + "[" + std::to_string(index) + "].has_emission", true);
-				setUniform(arrayName + "[" + std::to_string(index) + "].emissionTexture", tex->getTextureUnitNum());
-			}
-			else if (material.hasEmissionVector()) {
-				//ISSUE: constant emission vector is not supported, to be mended
-				setUniform(arrayName + "[" + std::to_string(index) + "].has_emission", false);
-			}
-		}
-	}
+	void setMaterial(const Material& material,int index, std::string arrayName = "materials");
 
 	/// <summary>
 	/// Same as setMaterial
@@ -289,8 +164,6 @@ public:
 	/// <param name="material">Material object to be set</param>
 	/// <param name="index">Index of material to be set in the shader</param>
 	/// <param name="arrayName">Name of the arra containing the materials in the shader.</param>
-	void setUniform(const Material& material, int index, std::string arrayName = "materials") {
-		setMaterial(material, index, arrayName);
-	}
+	void setUniform(const Material& material, int index, std::string arrayName = "materials");
 };
 
